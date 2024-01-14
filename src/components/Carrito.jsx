c2ea4c17752d8154a63ebd1bffbdb930216a7ebb
PanchoFrontEnd <francisco.2a.insuco1dp@gmail.com>
2024-01-14 04:04:25 -0300

    final del proyecto

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -14,23 +14,24 @@ const Carrito = () => {
         <h1 className="main-title">Carrito</h1>
   
         {carrito.map((prod, index) => (
-            <div key={`${prod.id}-${index}`}>
-                <h3>{prod.titulo}</h3>
-                <p>Precio unit: ${prod.precio}</p>
-                <p>Precio total: ${prod.precio * prod.cantidad}</p>
-                <p>Cant: {prod.cantidad}</p>
-            </div>
-    ))}
+    <div key={`${prod.id}-${index}`} className="producto">
+        <h3>{prod.titulo}</h3>
+        <p className="precio">Precio unit: ${prod.precio}</p>
+        <p className="precio">Precio total: ${prod.precio * prod.cantidad}</p>
+        <p className="cantidad">Cant: {prod.cantidad}</p>
+    </div>
+))}
+
+{carrito.length > 0 ? (
+    <>
+        <h2 className="precio-total">Precio total: ${precioTotal()} </h2>
+        <button className="vaciar-btn" onClick={handleVaciar}>Vaciar</button>
+        <Link to="/checkout" className="finalizar-compra-link">Finalizar compra</Link>
+    </>
+) : (
+    <h2 className="carrito-vacio">El carrito está vacío :(</h2>
+)}
   
-        {carrito.length > 0 ? (
-          <>
-            <h2>Precio total: ${precioTotal()} </h2>
-            <button onClick={handleVaciar}>Vaciar</button>
-            <Link to="/checkout">Finalizar compra</Link>
-          </>
-        ) : (
-          <h2>El carrito está vacío :(</h2>
-        )}
       </div>
     );
   };
