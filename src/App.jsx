688aaf08f259b830478eee52b11fbc32f1d2bc7a
PanchoFrontEnd <francisco.2a.insuco1dp@gmail.com>
2024-01-14 03:58:51 -0300

    Cambios Finales

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import './main.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Carrito from "./components/Carrito";
+import CheckOut from "./components/CheckOut";
  
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
           <Route path="/productos/:categoria" element={<ItemListContainer />} />
           <Route path="/nosotros" element={<Nosotros />}/>
           <Route path="/carrito" element={<Carrito />}/>
+          <Route path="/checkout" element={<CheckOut />}/>
         
         </Routes>
 
